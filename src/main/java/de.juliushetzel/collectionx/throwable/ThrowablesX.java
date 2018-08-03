74ee2a536235159c140f79259dd2745bd664644a
jhetzel <j.fr.hetzel@gmail.com>
2018-08-03 09:00:04 +0200

    [Fix] propagation method to not be ambigious

--- src/main/java/de.juliushetzel/collectionx/throwable/ThrowablesX.java
+++ src/main/java/de.juliushetzel/collectionx/throwable/ThrowablesX.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 
 public final class ThrowablesX {
 
-    public static <T> Predicate<T> propagate(ThrowingPredicate<T> predicate) {
+    public static <T> Predicate<T> propagatePredicate(ThrowingPredicate<T> predicate) {
         return (t) -> {
             try {
                 return predicate.test(t);
@@ -16,7 +16,17 @@ public final class ThrowablesX {
         };
     }
 
-    public static <T> Consumer<T> propagate(ThrowingConsumer<T> consumer) {
+    public static <T, E extends RuntimeException> Predicate<T> propagatePredicate(Function<Exception, E> runtimeException, ThrowingPredicate<T> predicate) {
+        return (t) -> {
+            try {
+                return predicate.test(t);
+            } catch (Exception e) {
+                throw runtimeException.apply(e);
+            }
+        };
+    }
+
+    public static <T> Consumer<T> propagateConsumer(ThrowingConsumer<T> consumer) {
         return (t) -> {
             try {
                 consumer.accept(t);
@@ -26,12 +36,22 @@ public final class ThrowablesX {
         };
     }
 
-    public static <T, R> Function<T, R> propagate(ThrowingFunction<T, R> function) {
+    public static <T, E extends RuntimeException> Consumer<T> propagateConsumer(Function<Exception, E> runtimeException, ThrowingConsumer<T> consumer) {
+        return (t) -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception e) {
+                throw runtimeException.apply(e);
+            }
+        };
+    }
+
+    public static <T, R, E extends RuntimeException> Function<T, R> propagateFunction(Function<Exception, E> runtimeException, ThrowingFunction<T, R> function) {
         return (t) -> {
             try {
                 return function.apply(t);
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                throw runtimeException.apply(e);
             }
         };
     }
